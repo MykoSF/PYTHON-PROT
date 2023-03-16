1a658a1ebad1ec7f7eb32f3d0debd4fb4eb07a7d
Maykon <mykosistema@gmail.com>
2023-03-16 17:04:11 -0300

    This repository will have a prototype of a Python age calculator

---
+++ pythonmath.py
@@ -0,0 +1,16 @@
+
+birthday = 2006
+name = 'Maykon'
+currentYear = 2023
+
+
+def agemath(birthday, currentYear):
+    age = currentYear - birthday
+    return age
+
+
+age = agemath(birthday, currentYear)
+
+print(age)
+
+
